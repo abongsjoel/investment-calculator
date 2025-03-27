5fb23ea676e1460b83bcfe1726e321e4ba55d889
Joel Chi <abongsjoel@gmail.com>
2025-03-27 23:51:35 +0100

    Add results component and move state up

---
+++ src/components/Results.jsx
@@ -0,0 +1,12 @@
+export default function Results({ input }) {
+  return (
+    <section id="results">
+      <h2>Results</h2>
+      <p>
+        Your investment will grow to{" "}
+        {/* <strong>${results.totalInvestment.toFixed(2)}</strong> in{" "}
+        <strong>{results.duration} years</strong>. */}
+      </p>
+    </section>
+  );
+}
